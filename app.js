f9d4369d00a7f3fde232df6350e27df7c034fca1
R.Florin <florin1994x@mail.ru>
2017-04-29 20:59:18 +0300

    Implementing registration & login

--- app.js
+++ app.js
@@ -1,84 +1,96 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var http = require("http");
-var fileSystem = require("fs");
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://localhost/BeeAlert-Thesis');
+var db = mongoose.connection;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+// Init App
 var app = express();
 
-/*
-// view engine setup
+// View Engine
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-*/
+app.engine('handlebars', exphbs({defaultLayout:'layout'}));
+app.set('view engine', 'handlebars');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+// BodyParser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Set Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.')
+            , root    = namespace.shift()
+            , formParam = root;
+
+        while(namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param : formParam,
+            msg   : msg,
+            value : value
+        };
+    }
+}));
+
+// Connect Flash
+app.use(flash());
+
+// Global Vars
+app.use(function (req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
+    next();
+});
+
+
 
 app.use('/', routes);
 app.use('/users', users);
 
-// specify the port
-var port = process.env.PORT || 3003;
+// Set Port
+app.set('port', (process.env.PORT || 3000));
 
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.listen(app.get('port'), function(){
+    console.log('Server started on port '+app.get('port'));
 });
-// error handlers
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+
+
+function check() {
+    console.log(mongoose.connection.readyState);
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-// create server at specified port
-var server = http.createServer(function(req, resp){
-   fileSystem.readFile('./views/index.html',function(error,fileContent){
-       if(error){
-           resp.writeHead(500,{'Content-Type':'text/plain'});
-           resp.end('Error');
-       }
-       else{
-           resp.writeHead(200,{'Content-Type':'text/html'});
-           resp.write(fileContent);
-           resp.end();
-       }
-   });
-});
-
-server.listen(port);
-console.log('Listening at: localhost:'+port);
-
-module.exports = app;
+var my_check = setTimeout(check,2000);

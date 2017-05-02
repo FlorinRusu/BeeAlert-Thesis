7776e93a72c1dd328b01fd8bf147e0252c84fb59
R.Florin <florin1994x@mail.ru>
2017-05-02 21:12:18 +0300

    Fix some UI-elements

---
+++ public/assets/js/switch.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Florin on 5/2/2017.
+ */
+
+
+
+var $contents = $('.tab-content');
+$contents.slice(1).hide();
+$('.tab').click(function() {
+    var $target = $('#' + this.id + 'show').show();
+    $contents.not($target).hide();
+});

32b450cd92ef4c16783a091c5b10687a38cc85f6
Enkey <fudo333@gmail.com>
2017-05-12 11:08:31 +0300

    feeds ajax (console)

--- public/assets/js/feeds.js
+++ public/assets/js/feeds.js
@@ -2,27 +2,27 @@
  * Created by Florin on 5/4/2017.
  */
 
-/*
+
     //feed to parse
 $(document).ready(function() {
 
     var feed = "http://feeds.feedburner.com/raymondcamdensblog?format=xml";
   //http://www.emsc-csem.org/service/rss/rss.php?filter=yes&min_mag=3.5&region=ROMANIA&min_intens=0&max_intens=8/
-    $.ajax(feed, {
-        accepts:{
-            xml:"application/rss+xml"
-        },
-        dataType:"xml",
+    $.ajax('/users/feeds', {
+        // accepts:{
+        //     xml:"application/rss+xml"
+        // },
+        dataType:"json",
         success:function(data) {
             //Credit: http://stackoverflow.com/questions/10943544/how-to-parse-an-rss-feed-using-javascript
-
-            $(data).find("item").each(function () { // or "item" or whatever suits your feed
-                var el = $(this);
-                console.log("------------------------");
-                console.log("title      : " + el.find("title").text());
-                console.log("link       : " + el.find("link").text());
-                console.log("description: " + el.find("description").text());
-            });
+            console.log(data);
+            // $(data).find("item").each(function () { // or "item" or whatever suits your feed
+            //     var el = $(this);
+            //     console.log("------------------------");
+            //     console.log("title      : " + el.find("title").text());
+            //     console.log("link       : " + el.find("link").text());
+            //     console.log("description: " + el.find("description").text());
+            // });
         },
 
         error: function( data){
@@ -31,4 +31,3 @@ $(document).ready(function() {
     });
 
 });
-*/

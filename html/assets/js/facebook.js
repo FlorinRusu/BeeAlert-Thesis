5d1c4c5962e2714e4a67e31240ac1e9204ed5031
R.Florin <florin1994x@mail.ru>
2017-07-16 12:06:28 +0300

    Recent changes on categories

--- html/assets/js/facebook.js
+++ html/assets/js/facebook.js
@@ -5,6 +5,6 @@
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
     js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/ro_RO/sdk.js#xfbml=1&version=v2.9";
+    js.src = "https://connect.facebook.net/ro_RO/sdk.js#xfbml=1&version=v2.9";
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
